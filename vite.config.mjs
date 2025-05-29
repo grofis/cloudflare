c4010e046233aa008f290224da3714d23a155678
Grofis He <1216226589@qq.com>
2025-05-29 12:55:29 +0800

    配置局域网访问

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,7 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 import { fileURLToPath, URL } from 'node:url'
 import path from 'path'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import fs from 'fs';
 
 export default defineConfig({
   plugins: [
@@ -34,9 +35,30 @@ export default defineConfig({
     }
   },
   server: {
+    https: {
+      key: fs.readFileSync('./src/assets/key/key.pem'),
+      cert: fs.readFileSync('./src/assets/key/cert.pem'),
+    },
     host: '0.0.0.0', // 或 host: '0.0.0.0'
     port: 5173,
+    disableHostCheck: true,
     headers: {
+      // 允许所有来源的 CORS 请求
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Credentials': 'true',
+      // 开发环境下最宽松的 CSP 配置
+      'Content-Security-Policy': `
+         default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;
+         img-src * data: blob: 'unsafe-inline';
+         connect-src * 'unsafe-inline';
+         script-src * 'unsafe-inline' 'unsafe-eval';
+         style-src * 'unsafe-inline';
+         font-src * data:;
+         frame-src *;
+       `.replace(/\s+/g, ' ').trim(),
+      // 禁用缓存，确保始终获取最新资源
       'Cache-Control': 'no-store',
     }
   }
