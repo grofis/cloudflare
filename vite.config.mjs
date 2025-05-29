a1fe8e7992e001162c4f0a2f626ac8f59e5e99cb
Grofis He <1216226589@qq.com>
2025-05-29 22:53:51 +0800

    本地开发的环境。

--- vite.config.mjs
+++ vite.config.mjs
@@ -35,10 +35,10 @@ export default defineConfig({
     }
   },
   server: {
-    https: {
-      key: fs.readFileSync('./src/assets/key/key.pem'),
-      cert: fs.readFileSync('./src/assets/key/cert.pem'),
-    },
+    // https: {
+    //   key: fs.readFileSync('./src/assets/key/key.pem'),
+    //   cert: fs.readFileSync('./src/assets/key/cert.pem'),
+    // },
     host: '0.0.0.0', // 或 host: '0.0.0.0'
     port: 5173,
     disableHostCheck: true,
@@ -48,16 +48,16 @@ export default defineConfig({
       'Access-Control-Allow-Methods': '*',
       'Access-Control-Allow-Headers': '*',
       'Access-Control-Allow-Credentials': 'true',
-      // 开发环境下最宽松的 CSP 配置
-      'Content-Security-Policy': `
-         default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;
-         img-src * data: blob: 'unsafe-inline';
-         connect-src * 'unsafe-inline';
-         script-src * 'unsafe-inline' 'unsafe-eval';
-         style-src * 'unsafe-inline';
-         font-src * data:;
-         frame-src *;
-       `.replace(/\s+/g, ' ').trim(),
+      // // 开发环境下最宽松的 CSP 配置
+      // 'Content-Security-Policy': `
+      //    default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;
+      //    img-src * data: blob: 'unsafe-inline';
+      //    connect-src * 'unsafe-inline';
+      //    script-src * 'unsafe-inline' 'unsafe-eval';
+      //    style-src * 'unsafe-inline';
+      //    font-src * data:;
+      //    frame-src *;
+      //  `.replace(/\s+/g, ' ').trim(),
       // 禁用缓存，确保始终获取最新资源
       'Cache-Control': 'no-store',
     }
